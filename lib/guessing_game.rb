2d5917b635cbb6e171f5800708412e3dcf8dea22
xiawei <graciexia@outlook.com>
2015-05-20 22:37:38 -0500

    basic working program ... with bugs :-\

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -1,34 +1,51 @@
 # class/Users/xiawei/my_projects/tiy/week3/day3/homework/lib/guessing_game.rb.
 
 class Guessing_game
-attr_accessor :random_number
 
-  def random
+  def initialize
+      start_game
+  end
 
+  def start_game
+    get_random_number
+    (1..5).each do |x|
+      get_guess
+      break if is_winner? == true
+    end
+  end
+
+  def get_random_number
     @random_number = (rand*100).to_i + 1
-    puts "The number is picked. Guess what is it? >"
+    puts "The number has been picked."
+  end
+
+  def get_guess
+    puts "Guess what is it? >"
     @action = gets.chomp.to_i
   end
 
-  def winning
-    @difference = @action - @random_number
-    if @difference > 0
+  def is_winner?
+    difference = @action - @random_number
+    if difference > 0
       case
-        when ((31..99).member? @difference) then puts "That is too high!"
-        when ((11..30).member? @difference) then puts "Closer,but still high!"
-        when ((1..10).member? @difference ) then puts "High, but so close!"
+        when ((31..99).member? difference) then puts "That is too high!"
+        when ((11..30).member? difference) then puts "Closer,but still high!"
+        when ((1..10).member? difference ) then puts "High, but so close!"
       end
-    elsif @difference < 0
+      return false
+    elsif difference < 0
       case
-        when ((-99..-31).member? @difference) then puts "That is too low!"
-        when ((-30..-11).member? @difference) then puts "Closer,but still low!"
-        when ((-10..-1).member? @difference) then puts "Low, but so close!"
+        when ((-99..-31).member? difference) then puts "That is too low!"
+        when ((-30..-11).member? difference) then puts "Closer,but still low!"
+        when ((-10..-1).member? difference) then puts "Low, but so close!"
       end
+      return false
     else
       puts "That is it. You win!"
     end
+    return true
   end
 
 end
 
-
+my_game = Guessing_game.new
