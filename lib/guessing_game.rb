5924842131bb0a757b56c388a1700325d583eab4
xiawei <graciexia@outlook.com>
2015-05-21 00:33:35 -0500

    fixed repeated input

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -3,20 +3,31 @@
 class Guessing_game
 
   def initialize
+    begin
       start_game
+      begin
+        print "Do you want to play again (y, n)? "
+        answer = gets.chomp.upcase
+      end until ["Y", "N"].member? answer
+    end until answer == "N"
   end
 
   def start_game
     get_random_number
+    @guesses = []
     (1..5).each do |x|
       get_guess
+      check_old_guesses
       break if is_winner? == true
+    end
+    if @action != @random_number
+      puts "Sorry, you only get 5 tries. You lose!"
     end
   end
 
   def get_random_number
     @random_number = (rand*100).to_i + 1
-    puts "The number has been picked."
+    puts "A number between 1 and 100 has been picked."
   end
 
   def get_guess
@@ -24,26 +35,26 @@ class Guessing_game
     @action = gets.chomp.to_i
   end
 
+  def check_old_guesses
+    if @guesses.include?(@action) == true
+      puts "Are you feeling all right?"
+    end
+    @guesses.push(@action)
+  end
+
   def is_winner?
     difference = @action - @random_number
-    if difference > 0
-      case
-        when ((31..99).member? difference) then puts "That is too high!"
-        when ((11..30).member? difference) then puts "Closer,but still high!"
-        when ((1..10).member? difference ) then puts "High, but so close!"
-      end
-      return false
-    elsif difference < 0
-      case
-        when ((-99..-31).member? difference) then puts "That is too low!"
-        when ((-30..-11).member? difference) then puts "Closer,but still low!"
-        when ((-10..-1).member? difference) then puts "Low, but so close!"
-      end
-      return false
+    if difference > 30 then puts "That is way too high!"
+    elsif difference > 10 then puts "That is close, but too high!"
+    elsif difference > 0 then puts "Very close, but too high!"
+    elsif difference < -30 then puts "That is way too low!"
+    elsif difference < -10 then puts "That is close, but too low!"
+    elsif difference < 0 then puts "Very close, but too low!"
     else
       puts "That is it. You win!"
+      return true
     end
-    return true
+    return false
   end
 
 end
